22556178cf54b5aa23e29120e35e86eb2081b422
Sanzida Hossain <sanzidahossain.dev@gmail.com>
2025-01-30 13:55:22 +0600

    print 1 to 10

---
+++ Assignment1.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fasterInOut ios::sync_with_stdio(false); cin.tie(0);
+
+int main() {
+    fasterInOut;
+
+    #ifndef ONLINE_JUDGE
+    freopen("C:/Users/USER1/Desktop/inout/input.txt", "r", stdin);
+    freopen("C:/Users/USER1/Desktop/inout/output.txt", "w", stdout);
+    #endif
+
+    for (int i = 1; i <= 10; i++)  // Fixed condition
+    {
+        cout << i << " ";
+    }
+
+    return 0;
+}
